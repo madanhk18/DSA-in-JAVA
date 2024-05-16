b15fdcccedbb683c8d49848de281ef1d329e6fc0
madan <madanhk18@gmail.com>
2024-05-16 19:28:22 +0530

    first one

---
+++ src/manoj/madanimp.java
@@ -0,0 +1,23 @@
+package manoj;
+
+
+import static madan.mhk.message;
+
+public class madanimp {
+
+    private final int no;
+    String papulation;
+
+    public madanimp(int no){
+        this.no=no;
+        this.papulation=papulation;
+
+    }
+    public static void main(String[] args) {
+        System.out.println("gud morning");
+        message();
+        madanimp sd= new madanimp(23);
+        sd.papulation= String.valueOf(34);
+        System.out.println();
+    }
+}
