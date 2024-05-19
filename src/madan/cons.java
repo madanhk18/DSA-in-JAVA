95dd6857bef7446870fa41505a47f8a2da1bcafb
madan <madanhk18@gmail.com>
2024-05-19 22:23:56 +0530

    may 19

--- src/madan/cons.java
+++ src/madan/cons.java
@@ -1,3 +1,5 @@
+package madan;
+
 class stud{
     int rol;
     String nam;
--- src/manoj/ex2.java
+++ src/manoj/ex2.java
@@ -1,2 +1,16 @@
-package manoj;public class ex2 {
+package manoj;
+
+public class ex2 {
+    public static void main(String[] args) {
+        ex1 one = new ex1(4,"Bruno",10000);
+        ex1 two = new ex1(4,"Bruno",10000);
+        ex1 three = new ex1(4,"Bruno",10900);
+
+        System.out.println(ex1.population);
+        greeting();
+    }
+    static void greeting()
+    {
+        System.out.println("Hi madan h k");
+    }
 }
