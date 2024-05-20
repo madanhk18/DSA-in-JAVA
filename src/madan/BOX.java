b262aa3773e0c44f34d187dcd0db687f6293f79b
madan <madanhk18@gmail.com>
2024-05-20 22:39:31 +0530

    May 20

--- src/madan/BOX.java
+++ src/madan/BOX.java
@@ -1,2 +1,33 @@
-package madan;public class BOX {
+package madan;
+
+class box_peri{
+    double length;
+    double bredth;
+    double weight;
+    double height;
+
+    public box_peri(double length, double bredth, double weight,double height) {
+        this.length = length;
+        this.bredth = bredth;
+        this.weight = weight;
+        this.height = height;
+    }
+}
+ class boxx extends box_peri {
+    double perimeter;
+
+    public boxx(double length, double bredth, double weight, double height,double perimeter) {
+        super(length,bredth,weight,height);
+        this.perimeter = perimeter;
+    }
+}
+
+public class BOX{
+    public static void main(String[] args) {
+        box_peri obj =new box_peri(3,5,6,7);
+        box_peri obj2 = new boxx(2,4,5,6,7);
+        System.out.println(obj.height);
+        System.out.println(obj2.length);
+
+    }
 }
