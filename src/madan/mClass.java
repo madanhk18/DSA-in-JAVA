dd2f0a9866246f6972fe3b395585eff283ce3dc8
madan <madanhk18@gmail.com>
2024-05-21 22:27:48 +0530

    may 21

---
+++ src/madan/mClass.java
@@ -0,0 +1,17 @@
+package madan;
+
+public class mClass
+{
+    protected int number =5;
+    String name;
+
+    public mClass(int number,String name) {
+        this.number = number;
+        this.name=name;
+    }
+
+    public void printt()
+    {
+        System.out.println("This is in the Main class");
+    }
+}
---
+++ src/manoj/subofm.java
@@ -0,0 +1,2 @@
+package manoj;public class subofm {
+}
