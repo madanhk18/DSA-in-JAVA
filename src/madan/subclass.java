8ae093f882ee15b91ea2af04bc8dd8a2f0004340
madan <madanhk18@gmail.com>
2024-06-01 22:25:47 +0530

    june 1

--- src/madan/subclass.java
+++ src/madan/subclass.java
@@ -1,2 +1,13 @@
-package madan;public class subclass {
+package madan;
+
+public class subclass extends mClass{
+    int weight;
+    public subclass(int number, String name,int weight) {
+        super(number, name);
+        this.weight=weight;
+    }
+    public void printt()
+    {
+        System.out.println("This is in the Sub class");
+    }
 }
