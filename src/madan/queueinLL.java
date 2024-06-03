e24c849c6c21833c56f5d740c7db626b083eaee2
madan <madanhk18@gmail.com>
2024-06-03 19:32:35 +0530

    june 3

--- src/madan/queueinLL.java
+++ src/madan/queueinLL.java
@@ -1,2 +1,22 @@
-package madan;public class queueinLL {
+package madan;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class queueinLL {
+    public static void main(String[] args) {
+        Queue<Integer> friends = new LinkedList<>();
+        friends.offer(99);
+       // System.out.println(friends.offer(66));
+        friends.offer(43);
+        friends.offer(75);
+      //  System.out.println(friends.peek());
+        System.out.println(friends.poll());
+        System.out.println(friends);
+        friends.remove();
+        System.out.println(friends);
+        friends.remove();
+        System.out.println(friends.poll());
+        System.out.println(friends.peek());
+
+    }
 }
