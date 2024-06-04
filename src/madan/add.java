c5f0d1a1b7c2811e2bc08575e6db51ec3e752d83
madan <madanhk18@gmail.com>
2024-06-04 08:56:57 +0530

    june 4

---
+++ src/madan/add.java
@@ -0,0 +1,13 @@
+package madan;
+
+public class add {
+    public static void main(String[] args) {
+        int a=4;
+        float b =4.01f;
+        if (a>b)
+        {
+            System.out.println("a is greater");
+        }
+        else System.out.println("b is grrter");
+    }
+}
