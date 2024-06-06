394b004fbd340422b504479e7ab92228eb59c5e5
madan <madanhk18@gmail.com>
2024-06-06 18:19:48 +0530

    june 6

--- src/madan/prioque.java
+++ src/madan/prioque.java
@@ -1,2 +1,22 @@
-package madan;public class prioque {
+package madan;
+
+import java.util.PriorityQueue;
+
+public class prioque {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+        pq.offer(22);
+        pq.offer(10);
+        pq.offer(49);
+        System.out.println(pq);
+        pq.offer(1);
+        pq.offer(88);
+        System.out.println(pq);
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+
+
+    }
 }
