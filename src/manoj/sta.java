0a3d369b9956ae37afa1b3fc98dc41420562bd47
madan <madanhk18@gmail.com>
2024-06-07 16:18:57 +0530

    june 7

--- src/manoj/sta.java
+++ src/manoj/sta.java
@@ -1,2 +1,16 @@
-package manoj;public class sta {
+package manoj;
+
+public class sta {
+    static int a=7;
+    static int b;
+
+    static {
+        System.out.println("I am gud");
+        b=a*4;
+    }
+
+    public static void main(String[] args) {
+        //sta obj =new sta();
+        System.out.println(sta.a + " "+ sta.b);
+    }
 }
