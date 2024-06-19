baa3136bec1ca9afaf5bdde0731f322709488a91
madan <madanhk18@gmail.com>
2024-06-19 21:26:12 +0530

    19

--- src/manoj/names.java
+++ src/manoj/names.java
@@ -1,2 +1,20 @@
-package manoj;public class names {
+package manoj;
+
+public class names {
+    static class a {
+        static String name;
+
+        public a(String name) {
+            a.name = name;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        a obj =new a("manoj");
+        a obj1 =new a("madan");
+        System.out.println(obj.name);
+        System.out.println(obj1.name);
+
+    }
 }
